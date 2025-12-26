10d91220df317c49128cc105d70fab9c94e5b351
Dipu Kumar <dipukumarpojhi@gmail.com>
2025-12-26 15:31:32 +0530

    vercel link saved

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { getToken, authFetch } from "../lib/auth";
 
-// const API = import.meta.env.VITE_API_URL || 'https://react-shiksak-sarthi.vercel.app'
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || 'https://react-shiksak-sarthi-d-bcib.vercel.app/'
+// const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default function Upload() {
   const [files, setFiles] = useState([]);
